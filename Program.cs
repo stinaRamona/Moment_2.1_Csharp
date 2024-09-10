5d40b0f89f9c52ad2ecbb5f57e31ad6bca5a276d
Stina Persson <stpe1901@student.miun.se>
2024-09-10 13:47:06 +0200

    lägger till repo på github

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿ 
+/* 
+Laboration 2.1 i kursen Programmering i C#.NET, HT24 Mittuniversitetet 
+Kod skriven av Stina Persson (stpe1901)
+Senast uppdaterad 10/9 -24 
+*/
+
+/*
+Lösningen ska vara ett program som tar emot ett inmatat datum (år-månad-dag) 
+och genererar veckodagen det datument förföll på
+*/ 
+
